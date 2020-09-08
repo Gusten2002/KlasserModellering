c677ed305529601edb710367523625ae77fb820a
August <august.agren@elev.ga.ntig.se>
2020-09-08 10:32:37 +0200

    Uppstart projekt

---
+++ KlasserModellering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KlasserModellering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
